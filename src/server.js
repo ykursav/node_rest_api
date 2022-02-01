4c2f2696e69f162268e6e88c281b47ad3fa1fef0
Yunus Emre Kursav <YUEKU@orsted.com>
2022-02-01 20:45:50 +0100

    People names changed.

---
+++ src/server.js
@@ -0,0 +1,39 @@
+import express from "express";
+import { people } from './people';
+import { promises as fs } from 'fs';
+import bodyParser from 'body-parser';
+
+let app = express();
+
+app.use(bodyParser.json());
+
+app.get("/hello", (req, res) => {
+    res.send("Hey!");
+});
+
+app.get("/people", (req, res) => {
+    res.json(people);
+});
+
+app.get("/people/:name", (req, res) => {
+    let { name } = req.params;
+    let person = people.find(x => x.name === name);
+    res.json(person);
+});
+
+app.get("/file-data", async (req, res) => {
+    let data = await fs.readFile(__dirname + "/people-data.json");
+    let people = JSON.parse(data);
+
+    res.json(people);
+});
+
+app.post("/people", (req, res) => {
+    let newPerson = req.body;
+    people.push(newPerson);
+    res.json(people);
+});
+
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+});
